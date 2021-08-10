e25a221cc5fd093ac773d4d90fab5f5f1c22b990
Teh Shou Luan <davidteh.0802@gmail.com>
2021-08-10 23:36:00 +0800

    added calculator

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import Calculator from './components/Calculator';
+import React from 'react';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
       <h1>Hello World</h1>
+      <h1>David</h1>
+      <div>
+      <React.StrictMode>
+          <Calculator/>
+      </React.StrictMode>
+    </div>
       </header>
     </div>
   );
